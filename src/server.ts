809f9cb4381ec8b7a6a55b05f0e8cc782c7f4999
Saeed <saeedissah@gmail.com>
2023-10-10 00:00:13 +0200

    First route and middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import cors from "kcors";
 import Koa from "koa";
+import Router from "koa-router";
 import koaBodyParser from "koa-bodyparser";
 import { logger } from "./logger";
 
 const serverPort = 9090;
 const requestTimeoutMs = 60000;
 const jsonLimit = "25mb";
+
+function setName(name: string) {}
 
 export const startServer = async () => {
   const app = new Koa();
@@ -16,6 +19,28 @@ export const startServer = async () => {
     })
   );
   app.use(koaBodyParser({ jsonLimit }));
+  app.use((ctx, next) => {
+    console.log("context", ctx);
+    next();
+  });
+
+
+  const router = new Router();
+  router.get("/", (ctx, next) => {
+    ctx.body = {
+      hello: "World!!!",
+    };
+    next();
+  });
+  router.get("/name", (ctx, next) => {
+    ctx.body = {
+      name: "Hassana Ahmed",
+    };
+    next();
+  });
+  app.use(router.routes());
+
+  
   const server = app.listen(serverPort);
   server.timeout = requestTimeoutMs;
 
